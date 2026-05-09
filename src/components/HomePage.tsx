8e596af9e8dbd41978ae6b7b6e34d78d7046c1d3
Jeison Alexis Rodríguez Angarita <jeyconalex123@gmail.com>
2026-05-08 19:20:45 -0500

    fix path imgs

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -98,7 +98,7 @@ export default function HomePage() {
         telefono: item.estudiante.telefono,
 
         foto: item.estudiante.foto
-          ? `${API_URL}/uploads/${item.estudiante.foto}`
+          ? `/uploads/${item.estudiante.foto}`
           : null,
 
         programas: item.programas.map((p: any) => ({
